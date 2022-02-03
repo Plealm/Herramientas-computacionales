ec1db8c15a3975da29f422de810050e76ee5369e
plealm <plealm@unal.edu.co>
2022-02-03 11:33:55 -0500

    Cambios en la tarea 18 del 16 de Diciembre

--- Tareas/Tarea18/tarea18.m
+++ Tareas/Tarea18/tarea18.m
@@ -3,37 +3,49 @@
 ## Triple pulso de la ecuación de difución
 ## con video
 
-clear all; close all; clc;
-numx = 101; numt = 2001; dx = 1/(numx -1);
-dt = 1.; D = 5e-6; D1 = 5e-6; D2 = 5e-6; 
-x = 0:dx:1; c = zeros(numx, numt); t(1) = 0;
-c(1,1) = 0; c(numx, 1) = 0; nu = .5; A = 0.9; nu1 = .25;
-mu = 0.5; sigma1 = 0.05; nu2 = .5; sigma2 = 0.02;
-nu3 = .75; sigma3 = 0.035;
+clear all;clc; close all;
 
+numx = 41; numy = numx; numt = 500;
+dx = 1/(numx - 1); dy = dx;dt = 0.001;
+D = 0.01;mu1 = .5; sigma = 0.05; mu2 = .7; 
+mu3 = .3; 
+x = 0:dx:1;y = 0:dy:1;
 
+C = zeros(numx, numy, numt);
+C(1:numx-1,1,1) = 0; C(1:numx-1,numy,1)=0;
+C(1,1:numy-1,1) = 5;C(numx,1:numy-1,1)=1;
+t(1) = 0; [X, Y]= meshgrid(x, y);
 
-for i = 2:numx-1;
-  c(i, 1) = exp(-(x(i) - nu1)^2/(2*sigma1^2))/sqrt(2*pi*sigma1^2) +...
-            exp(-(x(i) - nu2)^2/(2*sigma2^2))/sqrt(2*pi*sigma2^2) + ...
-            exp(-(x(i) - nu3)^2/(2*sigma3^2))/sqrt(2*pi*sigma3^2);
+for i = 2:numx-1
+  for j = 2:numy-1
+    C(i,j,1) = exp(-(x(i) - mu1)^(2)/(2*sigma^2)...
+                   -(y(j) - mu1)^(2)/(2*sigma^2))/...
+                   sqrt(2*pi*sigma^2) + ...
+                   exp(-(x(i) - mu2)^(2)/(2*sigma^2)...
+                   -(y(j) - mu2)^(2)/(2*sigma^2))/...
+                   sqrt(2*pi*sigma^2) + ...
+                   exp(-(x(i) - mu3)^(2)/(2*sigma^2)...
+                   -(y(j) - mu3)^(2)/(2*sigma^2))/...
+                   sqrt(2*pi*sigma^2); 
+  endfor
 endfor
-
-for j = 1:numt
-  t(j+1) = t(j) +dt;
-   for i= 2:numx-1
-       c(i,j+1) = c(i,j)+ D*(dt/dx^2)*(c(i+1,j)-2*c(i,j)+c(i-1,j));
-   end
-end
-fn = fullfile("Difusión.mp4");
+fn = fullfile("Difusion.mp4");
 w = VideoWriter(fn);
 open(w);
-figure(1); clf;
-for k=1:50:1001
-   plot(x, c(:, k), 'Linewidth', 2); hold on; grid on;
-   pause (0.1); drawnow;
+for j = 1:numt
+  t(j+1) = t(j) + dt;
+  for i = 2:numx-1
+    for k = 2:numy-1
+      C(i, k, j+1) = C(i, k,j) + (D*dt/dx^2)*...
+      (C(i+1, k, j) - 2*C(i, k, j) + C(i-1, k, j)) + ...
+      (D*dt/dy^2)*...
+      (C(i, k+1, j) - 2*C(i, k, j) + C(i, k-1, j));
+    endfor
+  endfor
+  figure(1);clf;mesh(X, Y, C(:, :, j)); drawnow;view(10,75); axis([0 1 0 1 0 10]);
+  axis off;
   writeVideo (w, getframe (gcf));
-end
-close(w)
+endfor
+close(w);
 
 
